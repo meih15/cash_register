c05535da017ad1f1bc6b107045585071be8cb59b
Mei Huang <meihuang8328@gmail.com>
2024-04-12 11:36:23 -0700

    cash register project setup

---
+++ drawer.js
@@ -0,0 +1,49 @@
+const drawer = [
+    {
+      name: 'penny',
+      value: 1, // a penny is 1 cent
+      quantity: 72 // 72 pennies in the drawer
+    },
+    {
+      name: 'nickel',
+      value: 5,
+      quantity: 41
+    },
+    {
+      name: 'dime',
+      value: 10,
+      quantity: 31
+    },
+    {
+      name: 'quarter',
+      value: 25,
+      quantity: 17
+    },
+    {
+      name: 'one', // n.b. a $1 bill is a note, not a coin
+      value: 100,
+      quantity: 90
+    },
+    {
+      name: 'five',
+      value: 500,
+      quantity: 11
+    },
+    {
+      name: 'ten',
+      value: 1_000,
+      quantity: 2
+    },
+    {
+      name: 'twenty',
+      value: 2_000,
+      quantity: 3
+    },
+    {
+      name: 'hundred',
+      value: 10_000,
+      quantity: 1
+    }
+  ]
+  
+  module.exports = drawer
---
+++ index.js
@@ -0,0 +1,65 @@
+const drawer = require('./drawer')
+// DO NOT EDIT CODE ABOVE
+
+// Level 1: removeItem and addItem
+
+function removeItem(name, drawer) {
+  // Write your code here
+
+
+}
+
+
+function addItem(name, drawer) {
+  // Write your code here
+
+}
+
+// Level 2: countCoins and countNotes
+function countCoins(drawer) {
+  // Write your code here
+
+
+}
+
+function countNotes(drawer) {
+  // Write your code here
+
+
+}
+
+// Level 3: sumDrawer
+
+function sumDrawer(drawer) {
+  // Write your code here
+
+
+}
+
+// Level 4: canMakeAmount
+
+function canMakeAmount(target, drawer) {
+  // Write your code here
+
+
+}
+
+// Level 5: transaction
+
+function transaction(cost, paid, drawer) {
+  // Write your code here
+
+
+
+}
+
+// DO NOT EDIT CODE BELOW
+// module.exports = {
+//   removeItem,
+//   addItem,
+//   countCoins,
+//   countNotes,
+//   sumDrawer,
+//   canMakeAmount,
+//   transaction
+// }
